8eec38e76ce1a60fa2119857784f45632ff7f224
NicolaSabino <nicola.sabino94@gmail.com>
2016-03-03 18:19:48 +0100

    controller impostazioni

--- src/Controller/ImpostazioniController.java
+++ src/Controller/ImpostazioniController.java
@@ -4,20 +4,54 @@ import Model.Utente;
 import View.Impostazioni;
 import View.RootFrame;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by nicola on 02/03/16.
  */
 public class ImpostazioniController {
+
     private RootFrame rootFrame;
     private Impostazioni impostazioni;
 
     public ImpostazioniController(RootFrame rootFrame, Utente utilizzatore) {
+
         this.rootFrame=rootFrame;
         this.impostazioni= new Impostazioni();
+
+        //imposto i campi dell'interfaccia
+        this.impostazioni.setCampoNome(utilizzatore.getNome());
+        this.impostazioni.setCampoCognome(utilizzatore.getCognome());
+        this.impostazioni.setCampoEmail(utilizzatore.getMail());
+        this.impostazioni.setCampoPassword(utilizzatore.getPwd());
+        this.impostazioni.setCampoTelefono(utilizzatore.getTelefono());
+
+        //setto tutti i campi come disabilitati
+        this.impostazioni.disabilitaCampi(true);
+
+        //imposto i listner
+        this.modificaListner();
+
         return;
     }
 
     public void apriImpostazioni(){
         rootFrame.setMainScrollPane(impostazioni.getPannelloPrincipale());
+    }
+
+    protected void modificaListner(){
+        JButton modifica = impostazioni.getButtonModifica();
+        modifica.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                modificaCampi();
+            }
+        });
+    }
+
+    protected void modificaCampi(){
+        this.impostazioni.disabilitaCampi(false);
     }
 }
--- src/View/Impostazioni.java
+++ src/View/Impostazioni.java
@@ -12,16 +12,72 @@ public class Impostazioni extends JPanel{
     private JTextField campoNome;
     private JTextField campoCognome;
     private JTextField campoEmail;
-    private JButton buttonEdit;
-    private JButton buttonSave;
+    private JButton buttonModifica;
+    private JButton buttonSalva;
     private JButton buttonLogout;
     private JPasswordField campoPassword;
+    private JTextField campoTelefono;
 
     public Impostazioni(){
         setVisible(true);
     }
 
+    public void disabilitaCampi(boolean controllo){
+
+        this.campoNome.setEnabled(!controllo);
+        this.campoCognome.setEnabled(!controllo);
+        this.campoEmail.setEnabled(!controllo);
+        this.campoPassword.setEnabled(!controllo);
+        this.campoTelefono.setEnabled(!controllo);
+        this.buttonSalva.setEnabled(!controllo);
+    }
+
+
+    //getter and setter
     public JPanel getPannelloPrincipale() {
         return pannelloPrincipale;
+    }
+
+    public void setPannelloPrincipale(JPanel pannelloPrincipale) {
+        this.pannelloPrincipale = pannelloPrincipale;
+    }
+
+    public JTextField getCampoNome() {
+        return campoNome;
+    }
+
+    public void setCampoNome(String campoNome) {
+        this.campoNome.setText(campoNome);
+    }
+
+
+    public void setCampoCognome(String campoCognome) {
+        this.campoCognome.setText(campoCognome);
+    }
+
+
+    public void setCampoEmail(String campoEmail) {
+        this.campoEmail.setText(campoEmail);
+    }
+
+
+    public void setCampoPassword(String campoPassword) {
+        this.campoPassword.setText(campoPassword);
+    }
+
+    public void setCampoTelefono(String campoTelefono){
+        this.campoTelefono.setText(campoTelefono);
+    }
+
+    public JButton getButtonModifica() {
+        return buttonModifica;
+    }
+
+    public JButton getButtonSalva() {
+        return buttonSalva;
+    }
+
+    public JButton getButtonLogout() {
+        return buttonLogout;
     }
 }
